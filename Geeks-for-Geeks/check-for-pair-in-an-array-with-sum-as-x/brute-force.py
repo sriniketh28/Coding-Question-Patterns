2c5356e0a644ca967ccb351b090b6a354a2ad379
Muppaneni Sriniketh <39892279+sriniketh28@users.noreply.github.com>
2021-04-27 12:18:19 +0530

    Update brute-force.py

--- Geeks-for-Geeks/check-for-pair-in-an-array-with-sum-as-x/brute-force.py
+++ Geeks-for-Geeks/check-for-pair-in-an-array-with-sum-as-x/brute-force.py
@@ -3,7 +3,7 @@ def two_numbers_of_array(arr, x):
     for i in range(0,len(arr)):
         for j in range(i+1,len(arr)):
             if arr[i]+arr[j]==x:
-                // temp to store two or three possiblities like in this case two_numbers_of_array([0, -1, 2, -3, 1,-4],-2) output is [[2, -4], [-3, 1]]
+                # temp to store two or three possiblities like in this case two_numbers_of_array([0, -1, 2, -3, 1,-4],-2) output is [[2, -4], [-3, 1]]
                 temp = []
                 temp.append(arr[i])
                 temp.append(arr[j])
