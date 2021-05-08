4fe4b51e3be64ab03382ec7734ed055603f1f39c
Muppaneni Sriniketh <39892279+sriniketh28@users.noreply.github.com>
2021-05-08 12:02:00 +0530

    Create solution.py

---
+++ Pattern-Fast_Slow-pointers/Middle-of-the-LinkedList/solution.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def find_middle_of_linked_list(head):
+    slow = fast = head
+    while fast is not None and fast.next is not None:
+        slow = slow.next
+        fast = fast.next.next
+    return slow
+
+
+head = Node(1)
+head.next = Node(2)
+head.next.next = Node(3)
+head.next.next.next = Node(4)
+head.next.next.next.next = Node(5)
+
+print(find_middle_of_linked_list(head).data)
