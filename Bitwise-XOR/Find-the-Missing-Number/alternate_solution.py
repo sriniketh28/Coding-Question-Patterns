2103ee7b69360559e50beac6032ad40622b7b5e5
Muppaneni Sriniketh <39892279+sriniketh28@users.noreply.github.com>
2021-05-09 22:42:27 +0530

    Delete Bitwise-XOR/Find-the-Missing-Number directory

--- Bitwise-XOR/Find-the-Missing-Number/alternate_solution.py
+++
@@ -1,6 +0,0 @@
-def find_missing_number(nums):
-    return (len(nums)*(len(nums)+1))//2 - sum(nums)
-
-print(find_missing_number([4,0,3,1]))
-print(find_missing_number([8, 3, 5, 2, 4, 6, 0, 1]))
-print(find_missing_number([0, 1]))
